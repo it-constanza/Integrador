76e2a75ad19a72ca8cb8e144bc6aac00b06b8441
Cony Feldman <it.constanza@gmail.com>
2023-09-19 21:23:28 -0300

    Agrego Footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,19 +6,19 @@ const Navbar = () => {
   return (
 
     <nav className="nav-bar"> 
-      <div className="nav-bar__logo-container"> <img src="/img/logo.png" alt="logo" /></div>
+      <div className="nav-bar__logo-container"> <NavLink to="/">  <img src="/img/logo.png" alt="logo" /> </NavLink> </div>
       <ul className="nav-bar__nav-list">
         <li className="nav-bar__nav-item">
-          <NavLink to="#" className="nav-bar__nav-link">Inicio</NavLink>
+          <NavLink to="/" className="nav-bar__nav-link">Inicio</NavLink>
         </li>
         <li className="nav-bar__nav-item">
-          <NavLink to="views/NavLinklta.html" className="nav-bar__nav-link">Alta</NavLink>
+          <NavLink to="/alta" className="nav-bar__nav-link">Alta</NavLink>
         </li>
         <li className="nav-bar__nav-item">
-          <NavLink to="views/contacto.html" className="nav-bar__nav-link">Contacto</NavLink>
+          <NavLink to="/contacto" className="nav-bar__nav-link">Contacto</NavLink>
         </li>
         <li className="nav-bar__nav-item">
-          <NavLink to="views/nosotros.html" className="nav-bar__nav-link">Nosotros</NavLink>
+          <NavLink to="/nosotros" className="nav-bar__nav-link">Nosotros</NavLink>
         </li>
       </ul>
     </nav>
