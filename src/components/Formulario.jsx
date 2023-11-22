3a8f290ad87b07842ac4613a1c65ae10e82bb719
Cony Feldman <it.constanza@gmail.com>
2023-11-21 21:21:32 -0300

    alta form con drag & Drop

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react'
 import ProductoContext from '../contexts/ProductoContext'
 import { useForm } from '../hooks/useForm'
+import DragDrop from './DragDrop'
 
 const formInicial = {
   id: null,
@@ -15,11 +16,22 @@ const formInicial = {
 }
 
 const Formulario = ({ productoAEditar, setProductoAEditar }) => {
+  // Estados del drag and drop
+  const [foto, setFoto] = useState('')
+  const [srcImagen, setSrcImagen] = useState('')
+
   const [form, setForm, handleChange] = useForm(formInicial)
   const { crearProductoContext, actualizarProductoContext } = useContext(ProductoContext)
 
   useEffect(() => {
-    productoAEditar ? setForm(productoAEditar) : setForm(formInicial)
+    if (productoAEditar) {
+      setSrcImagen(productoAEditar.foto)
+      setForm(productoAEditar)
+    } else {
+      setForm(formInicial)
+    }
+
+    // productoAEditar ? setForm(productoAEditar) : setForm(formInicial)
   }, [productoAEditar, setProductoAEditar])
 
 
@@ -27,9 +39,11 @@ const Formulario = ({ productoAEditar, setProductoAEditar }) => {
     e.preventDefault() // Detener el comportamiento del formulario
     try {
       if (form.id === null) {
-        await crearProductoContext(form)
+        const productoNuevo = { ...form, ...foto }
+        await crearProductoContext(productoNuevo)
       } else {
-        await actualizarProductoContext(form)
+        const productoEditado = { ...form, ...foto }
+        await actualizarProductoContext(productoEditado)
       }
       handleReset()
     } catch (error) {
@@ -41,6 +55,7 @@ const Formulario = ({ productoAEditar, setProductoAEditar }) => {
   const handleReset = () => {
     setForm(formInicial)
     setProductoAEditar(null)
+    setSrcImagen('')
   }
 
 
@@ -48,45 +63,48 @@ const Formulario = ({ productoAEditar, setProductoAEditar }) => {
     <>
       <h3>{productoAEditar ? 'Editando' : 'Agregando'}</h3>
 
-      <form onSubmit={handleSubmit} >
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="lbl-nombre">Nombre</label>
+          <input type="text" name="nombre" id="lbl-nombre" value={form.nombre} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="lbl-precio">Precio</label>
+          <input type="text" name="precio" id="lbl-precio" value={form.precio} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="lbl-stock">Stock</label>
+          <input type="text" name="stock" id="lbl-stock" value={form.stock} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="lbl-marca">Marca</label>
+          <input type="text" name="marca" id="lbl-marca" value={form.marca} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="lbl-categoria">Categoría</label>
+          <input type="text" name="categoria" id="lbl-categoria" value={form.categoria} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="lbl-detalles">Detalles</label>
+          <input type="text" name="detalles" id="lbl-detalles" value={form.detalles} onChange={handleChange} />
+        </div>
+        {/*  <div>
+          <label htmlFor="lbl-foto">Foto</label>
+          <input type="text" name="foto" id="lbl-foto" value={form.foto} onChange={handleChange} />
+        </div> */}
 
-        <div className="edit-form">
-          <div>
-            <label htmlFor="lbl-nombre">Nombre</label>
-            <input type="text" name="nombre" id="lbl-nombre" value={form.nombre} onChange={handleChange} />
-          </div>
-          <div>
-            <label htmlFor="lbl-precio">Precio</label>
-            <input type="text" name="precio" id="lbl-precio" value={form.precio} onChange={handleChange} />
-          </div>
-          <div>
-            <label htmlFor="lbl-stock">Stock</label>
-            <input type="text" name="stock" id="lbl-stock" value={form.stock} onChange={handleChange} />
-          </div>
-          <div>
-            <label htmlFor="lbl-marca">Marca</label>
-            <input type="text" name="marca" id="lbl-marca" value={form.marca} onChange={handleChange} />
-          </div>
-          <div>
-            <label htmlFor="lbl-categoria">Categoría</label>
-            <input type="text" name="categoria" id="lbl-categoria" value={form.categoria} onChange={handleChange} />
-          </div>
-          <div>
-            <label htmlFor="lbl-detalles">Detalles</label>
-            <input type="text" name="detalles" id="lbl-detalles" value={form.detalles} onChange={handleChange} />
-          </div>
-          <div>
-            <label htmlFor="lbl-foto">Foto</label>
-            <input type="text" name="foto" id="lbl-foto" value={form.foto} onChange={handleChange} />
-          </div>
-          <div>
-            <label htmlFor="lbl-envio">Envío</label>
-            <input type="checkbox" name="envio" id="lbl-envio" checked={form.envio} onChange={handleChange} />
-          </div>
-        </div> {/* fin class edit-form */}
-        <div className="botones">
-          <button type="submit">Guardar</button>
-          <button type="reset" onClick={handleReset}>Limpiar</button></div>
+        <DragDrop
+          setFoto={setFoto}
+          setSrcImagen={setSrcImagen}
+          srcImagen={srcImagen} />
+
+        <div>
+          <label htmlFor="lbl-envio">Envío</label>
+          <input type="checkbox" name="envio" id="lbl-envio" checked={form.envio} onChange={handleChange} />
+        </div>
+
+        <button type="submit">Guardar</button>
+        <button type="reset" onClick={handleReset}>Limpiar</button>
       </form>
     </>
   )
